9047cf246fcd9e1f962203f64c906bb1f13863b3
lydia tschoe <lydiatschoe@gmail.com>
2023-06-09 11:14:25 -0400

    style.css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+console.log('Script loaded successfully!');
+
+// Smooth scrolling for anchor links within the page
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
